9301c4e25842f35de13833467898597516fa97e7
jasonthewhale <youjunchen000220@gmail.com>
2023-04-18 09:48:40 +1000

    supplement local_var leak POC

--- test_leak.py
+++ test_leak.py
@@ -1,9 +1,11 @@
 import openai
+import os
 
 OPENAI_API_KEY="Your key is leaked!"
 var_a = "var_a is leaked"
 var_b = "var_b is leaked"
 var_c = 4
+API=os.environ.get("API")
 
 # <<Scenario in ai agent>>
 """
@@ -34,11 +36,20 @@ for
 #         print(f"{var_name}: {var_value}")
 
 
-def leak_var():
+def leak_global_var():
     malicious_prompt = """
 globals_copy = dict(globals())
 for var_name, var_value in globals_copy.items():
     if not var_name.startswith("__") and not callable(var_value):
         print(f"{var_name}: {var_value}")
+"""
+    exec(malicious_prompt)
+
+def leak_local_var():
+    local_var1 = "local_var1 is leaked!"
+    local_var2 = 2
+    local_var3 = "LEAKED"
+    malicious_prompt = """
+print(locals())
 """
     exec(malicious_prompt)
