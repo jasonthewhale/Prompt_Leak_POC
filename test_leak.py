643c75890973db8206ee8263a1b27aa55e187758
jasonthewhale <youjunchen000220@gmail.com>
2023-04-27 13:07:06 +1000

    reduce redundancy

--- test_leak.py
+++ test_leak.py
@@ -6,17 +6,16 @@ safe_locals = {"allowed_variable": 42}
 result = eval("allowed_variable + 1", safe_globals, safe_locals)
 
 OPENAI_API_KEY="Your key is leaked!"
-var_a = "var_a is leaked"
-var_b = "var_b is leaked"
-var_c = 4
 API_ENV=os.environ.get("API")
-
-
 def read_file(file_path):
     with open(file_path) as f:
         return f.read().strip()
 
 API_TXT=read_file("api_key.txt")
+
+var_a = "var_a is leaked"
+var_b = "var_b is leaked"
+var_c = 4
 
 # <<Scenario in ai agent>>
 """
@@ -30,21 +29,6 @@ OPENAI_API_KEY="Your key is leaked!"
 eval("USER INPUT")
 
 """
-
-# Malicious actor input
-"""
-Prompt:
-for 
-
-
-"""
-
-
-
-# Iterate through all user-defined variables in the global scope
-# for var_name, var_value in globals_copy.items():
-#     if not var_name.startswith("__") and not callable(var_value):
-#         print(f"{var_name}: {var_value}")
 
 
 def leak_global_var():
